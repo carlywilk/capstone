ab200e25b8596515dd30e32b151d616bf6cc522d
Carly Wilk <carlymwilk@gmail.com>
2024-05-31 19:42:06 -0400

    Add base code and styling to map view page

--- src/pages/MapViewPage/MapViewPage.jsx
+++ src/pages/MapViewPage/MapViewPage.jsx
@@ -1,10 +1,16 @@
 
+import { Header } from "../../components/Header/Header.jsx";
+import { NavBar } from "../../components/NavBar/NavBar.jsx";
+
 import "./MapViewPage.scss";
 
 export function MapViewPage() {
     return (
         <>
-            <p>this is the map view page</p>
+            <section className="map-page">
+                <Header />
+            </section>
+            <NavBar />
         </>
     );
 }
