6df25fb05a2c4047dea3193e119327b8880853cc
Carly Wilk <carlymwilk@gmail.com>
2024-05-31 19:38:43 -0400

    Create and fill with lorem

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,10 +1,18 @@
 
+import { Header } from "../../components/Header/Header";
+import { NavBar } from "../../components/NavBar/NavBar";
+
 import "./AboutPage.scss";
 
 export function AboutPage() {
     return (
         <>
-            <p>this is the about page</p>
+            <section className="about-page">
+                <Header />
+                <div className="about-page__creator-info">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo reprehenderit excepturi tenetur maiores voluptatem ullam architecto? Ut, nesciunt libero! Sunt, iste impedit? Veniam, non distinctio repellat aut numquam itaque adipisci!</div>
+                <div className="about-page__emergency">Lorem ipsum dolor sit amet consectetur adipisicing elit.</div>
+            </section>
+            <NavBar />
         </>
     );
 }
