549b5299eb71210a452800be2220f37de920b24e
Carly Wilk <carlymwilk@gmail.com>
2024-06-02 03:24:17 -0400

    Fix listview page so that mobile to desktop code and styling is DRY

--- src/pages/ListViewPage/ListViewPage.jsx
+++ src/pages/ListViewPage/ListViewPage.jsx
@@ -1,18 +1,29 @@
+import { AboutApp } from "../../components/AboutApp/AboutApp.jsx";
 import { Header } from "../../components/Header/Header.jsx";
 import { ResourceAccordian } from "../../components/ResourceAccordian/ResourceAccordian.jsx";
+import { SearchBar } from "../../components/SearchBar/SearchBar.jsx";
 import { NavBar } from "../../components/NavBar/NavBar.jsx";
 
 import "./ListViewPage.scss";
 
 export function ListViewPage({ resourceList }) {
-
     return (
-        <>
-            <section className="list">
-                <Header />
-                <ResourceAccordian resourceList={resourceList} />
-            </section>
-            <NavBar />
-        </>
-    );
+        <section className="list">
+            <main className="list__main">
+                <section className="list__sidebar">
+                    <div className="list__sidebar--padded">
+                        <Header />
+                        <div className="list__sidebar--hide">
+                            <AboutApp />
+                        </div>
+                        <SearchBar />
+                    </div>
+                    <NavBar />
+                </section>
+                <section className="list__resources">
+                    <ResourceAccordian resourceList={resourceList} />
+                </section>
+            </main>
+        </section>
+    )
 }
