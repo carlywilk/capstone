218507f9764735912e1b567f3b7971675cdcffe5
Carly Wilk <carlymwilk@gmail.com>
2024-06-02 03:35:13 -0400

    Fix mapviewpage jsx and styling to be DRY

--- src/pages/MapViewPage/MapViewPage.jsx
+++ src/pages/MapViewPage/MapViewPage.jsx
@@ -1,23 +1,29 @@
-
+import { AboutApp } from "../../components/AboutApp/AboutApp.jsx";
 import { Header } from "../../components/Header/Header.jsx";
-import { NavBar } from "../../components/NavBar/NavBar.jsx";
 import { ResourceMap } from "../../components/ResourceMap/ResourceMap.jsx";
-// import { Sidebar } from "../../components/Sidebar/Sidebar.jsx";
+import { SearchBar } from "../../components/SearchBar/SearchBar.jsx";
+import { NavBar } from "../../components/NavBar/NavBar.jsx";
 
 import "./MapViewPage.scss";
 
 export function MapViewPage() {
     return (
-        <>
-            <section className="map-page">
-                <div className="map-page__header-mobile">
-                    <Header />
-                </div>
-                <ResourceMap />
-            </section>
-            <section className="map-page__nav-mobile">
-                <NavBar />
-            </section>
-        </>
-    );
+        <section className="map-page">
+            <main className="map-page__main">
+                <section className="map-page__sidebar">
+                    <div className="map-page__sidebar--padded">
+                        <Header />
+                    <div className="list__sidebar--hide">
+                        <AboutApp />
+                    </div>
+                    <SearchBar />
+                    </div>
+                    <NavBar />
+                </section>
+                <section className="map-page__map">
+                    <ResourceMap />
+                </section>
+            </main>
+        </section>
+    )
 }
